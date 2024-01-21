23f42657425052c9a8e503b320832aa5933c1090
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-01-22 01:51:13 +0200

    Fix home controller issue

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,13 +2,11 @@ const router = require('express').Router();
 
 const movieService = require('../services/movieService');
 
+
 router.get('/', (req,res) => {
     const movies = movieService.getAll();
-})
 
-
-router.get('/', (req,res) => {
-    res.render('home', {movies});
+    res.render('home', { movies });
 });
 
 router.get('/about', (req,res) => {
