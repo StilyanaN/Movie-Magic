da0dcefce73c56dff5792068413093972ac30f2a
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-01-22 01:21:03 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,12 @@
-const movies = [];
+const movies = [{
+    title: 'Jungle Cruise',
+    genre: 'Adventure',
+    director: 'Spilberg',
+    year: '2019',
+    imageUrl: '/img/jungle-cruise.jpeg',
+    rating: '5',
+    description: 'Dreaming about saving countless lives and having another adventure'
+}];
 
 exports.create = (movieData) => {
     movies.push(movieData)
