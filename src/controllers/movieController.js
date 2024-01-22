18a33bef933c5907fb58280e4a871788c93f375f
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-01-22 02:23:18 +0200

    Add details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -4,14 +4,22 @@ const movieService = require('../services/movieService');
 
 router.get('/create', (req, res) =>{
     res.render('create');
-})
+});
 
 router.post('/create', (req,res) =>{
     const newMovie = req.body;
 
     movieService.create(newMovie);
 
-    res.send('Movie should be created')
-})
+    res.redirect('/');
+});
+
+router.get('/movies/:movieId', (req,res) => {
+    const movieId = req.params.movieId;
+    const movie = movieService.getOne(movieId)
+    res.render('details', {movie})
+
+});
+
 
 module.exports = router;
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -13,6 +13,10 @@ exports.getAll = () => {
     return movies.slice();
 }
 
+exports.getOne = (movieId) => {
+    return movies.find(movie => movie._id == movieId);
+}
+
 exports.create = (movieData) => {
     movieData._id = movies[movies.length -1]._id + 1; 
     movies.push(movieData)
