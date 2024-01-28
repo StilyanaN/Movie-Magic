a4f75957fb63dd92a61179cea481fce5b1760be4
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-01-28 02:13:48 +0200

    Fix search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,22 +3,24 @@ const Movie = require('../models/Movie');
 
 exports.getAll = () => Movie.find()
 
-exports.search = async (title, genre, year) =>{
-    let result = await Movie.find().lean();
+exports.search = (title, genre, year) =>{
+    let query = {};
 
     if(title){
-        result = result.filter(movie => movie.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()));
+       
+       query.title =  new RegExp(title, 'i');
     }
 
     if(genre){
-        result = result.filter(movie => movie.genre.toLocaleLowerCase() === genre.toLocaleLowerCase());
+       
+        query.genre = genre.toLowerCase();
     }
 
     if(year){
-        result = result.filter(movie => movie.year === year);
+        query.year = year ;
     }
 
-    return result;
+    return Movie.find(query);
 
 };
 
