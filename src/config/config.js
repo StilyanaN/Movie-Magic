771fe59d2406309954780de437a6876933458968
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-02-04 16:49:51 +0200

    Add route guard

---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    SECRET: 'njgsjdsjdsb453gfgh',
+}
--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,9 +1,10 @@
+const util = require('util');
 const jwt = require('jsonwebtoken');
 
-function sign(payload, secretOrPrivateKey, options = {}){
-    const promise = new Promise((resolve,reject)=>{
-        jwt.sign(payload, secretOrPrivateKey,options,(err, token) => {
-            if(err){
+function sign(payload, secretOrPrivateKey, options = {}) {
+    const promise = new Promise((resolve, reject) => {
+        jwt.sign(payload, secretOrPrivateKey, options, (err, token) => {
+            if (err) {
                 return reject(err);
             }
             resolve(token);
@@ -12,6 +13,9 @@ function sign(payload, secretOrPrivateKey, options = {}){
     return promise;
 }
 
+const verify = util.promisify(jwt.verify);
+
 module.exports = {
     sign,
-}
+    verify,
+};
