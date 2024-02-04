0b30ca9dadc2319d13307b520f5ad6aed56808dc
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-02-04 17:22:13 +0200

    Add conditional edit and delete buttons on details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -7,10 +7,11 @@ const { isAuth } = require('../middlewares/authMiddleware');
 router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
+    const isOwner = movie.owner == req.user._id;
 
     movie.rating = new Array(Number(movie.rating)).fill(true);
 
-    res.render('movie/details', { movie });
+    res.render('movie/details', { movie, isOwner });
 });
 
 
