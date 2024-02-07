1ffbe30424895526a44000e490966f34c1d118e5
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-02-08 00:35:51 +0200

    Add error handling and validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,21 +1,25 @@
 const router = require('express').Router();
 
+const User = require('../models/User');
 const authService = require('../services/authService');
+const { getErrorMessage, validate } = require('../utils/errorUtils');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', validate(User), async (req, res) => {
     const userData = req.body;
 
     try {
         await authService.register(userData);
+
         res.redirect('/auth/login');
     } catch (err) {
-        res.render('auth/register', { error: err.message })
-    }
+        const message = getErrorMessage(err);
 
+        res.render('auth/register', { ...userData, error: message });
+    }
 });
 
 router.get('/login', (req, res) => {
@@ -25,17 +29,23 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
-    const token = await authService.login(email, password);
+    try {
+        const token = await authService.login(email, password);
 
-    res.cookie('auth', token);
+        res.cookie('auth', token);
 
-    res.redirect('/');
+        res.redirect('/');
+    } catch (err) {
+        const message = getErrorMessage(err);
+
+        res.status(400).render('auth/login', { error: message });
+    }
 });
 
 router.get('/logout', (req, res) => {
     res.clearCookie('auth');
 
     res.redirect('/');
-})
+});
 
 module.exports = router;
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,12 +3,13 @@ const router = require('express').Router();
 const movieService = require('../services/movieService');
 const castService = require('../services/castService');
 const { isAuth } = require('../middlewares/authMiddleware');
+const { getErrorMessage } = require('../utils/errorUtils');
 
 router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
     const isOwner = movie.owner == req.user?._id;
-    
+
 
     movie.rating = new Array(Number(movie.rating)).fill(true);
 
@@ -25,19 +26,20 @@ router.post('/create', isAuth, async (req, res) => {
         ...req.body,
         owner: req.user._id,
     }
-    
+
     try {
         await movieService.create(newMovie);
 
         res.redirect('/');
     } catch (err) {
-        console.log(err.message)
-        res.redirect('/create')
+        const message = getErrorMessage(err);
+
+        res.status(400).render('create', { error: message, ...newMovie });
     }
 });
 
 
-router.get('/movies/:movieId/attach',isAuth, async (req, res) => {
+router.get('/movies/:movieId/attach', isAuth, async (req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean();
     const casts = await castService.getAll().lean()
 
@@ -45,9 +47,9 @@ router.get('/movies/:movieId/attach',isAuth, async (req, res) => {
     res.render('movie/attach', { ...movie, casts });
 });
 
-router.post('/movies/:movieId/attach',isAuth, async (req, res) => {
+router.post('/movies/:movieId/attach', isAuth, async (req, res) => {
     const castId = req.body.cast;
-    const movieId = req.params.movieId; 
+    const movieId = req.params.movieId;
 
     await movieService.attach(movieId, castId);
 
@@ -60,14 +62,14 @@ router.get('/movies/:movieId/edit', isAuth, async (req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean();
     res.render('movie/edit', { movie });
 });
-router.post('/movies/:movieId/edit', isAuth, async (req,res) => {
-const editedMovie = req.body;
+router.post('/movies/:movieId/edit', isAuth, async (req, res) => {
+    const editedMovie = req.body;
 
-await movieService.edit(req.params.movieId, editedMovie);
-res.redirect(`/movies/${req.params.movieId}`)
+    await movieService.edit(req.params.movieId, editedMovie);
+    res.redirect(`/movies/${req.params.movieId}`)
 })
 
-router.get('/movies/:movieId/delete', isAuth, async(req,res) => {
+router.get('/movies/:movieId/delete', isAuth, async (req, res) => {
     await movieService.delete(req.params.movieId);
 
     res.redirect('/')
